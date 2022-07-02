802db863223185045478165af7f6fcc170e8ab2f
Aliwahid17 <88263575+Aliwahid17@users.noreply.github.com>
2022-07-02 18:45:49 +0530

    this is fast approach of solving armstrong number

--- Armstrong_number.py
+++ Armstrong_number.py
@@ -1,13 +1,14 @@
-# checking for armstrong number
-a = input("Enter a number")
-n = int(a)
-S = 0
-while n > 0:
-    d = n % 10
-    S = S + (d**3)
-    # There was an error here, it is not n = n / 10, it is n = n // 10
-    n = n // 10
-if int(a) == S:
-    print("Armstrong Number")
-else:
-    print("Not an Armstrong Number")
+# this code solve armstrong number problem by creating a function and it return true and false
+def is_armstrong_number(number):
+    numberstr = str(number)
+    length = len(numberstr)
+    num = number
+    rev = 0
+    temp = 0
+    while num != 0:
+        rem = num % 10
+        num //=  10
+        temp += rem ** length
+    return temp == number
+
+is_armstrong_number(5)
